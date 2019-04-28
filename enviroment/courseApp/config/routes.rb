b3532ee9040a8a1e861da95a2f99f7179755694e
Shahrzad Rafezi <shahrzadrafezi@192-168-1-103.tpgi.com.au>
2019-04-28 11:27:15 +1000

    Added the backend stuff for sign up.

--- enviroment/courseApp/config/routes.rb
+++ enviroment/courseApp/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'users/new'
   get '/courses', to: 'static_pages#courses'
   get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
--- enviroment/courseApp/db/schema.rb
+++ enviroment/courseApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_082819) do
+ActiveRecord::Schema.define(version: 2019_04_28_001708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_04_24_082819) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
