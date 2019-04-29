8c94af0ef3acfa079e5a9aa9e8c169ff0fd7a47a
Shahryar Faraji <shahryar@1receipt.io>
2019-04-30 09:15:20 +1000

    updated the contact form

--- enviroment/courseApp/config/routes.rb
+++ enviroment/courseApp/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/courses', to: 'static_pages#courses'
+  get  '/courses', to: 'static_pages#courses'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
