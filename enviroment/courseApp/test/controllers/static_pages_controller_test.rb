56bab5df5662fbb5c8341e6f610e30abfb6becbd
Shahrzad Rafezi <shahrzadrafezi@Shahrzads-MacBook-Pro.local>
2019-04-30 14:09:38 +1000

    fixed static controller test

--- enviroment/courseApp/test/controllers/static_pages_controller_test.rb
+++ enviroment/courseApp/test/controllers/static_pages_controller_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Rapid Application Development Assignment"
+    @base_title = "courseApp"
   end
 
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "courseApp | #{@base_title}"
+    assert_select "title", "#{@base_title} | Rapid Application Development Assignment"
   end
 
   test "should get course" do
     get courses_path
     assert_response :success
-    assert_select "title", "Course | #{@base_title}"
+    assert_select "title", "#{@base_title} | Course"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "#{@base_title} | Contact"
   end
 
 end
