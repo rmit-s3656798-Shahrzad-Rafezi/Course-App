db1ca03cb9f3aa618d8fa0400a85e8f35e813129
Shahryar Faraji <shahryar@1receipt.io>
2019-05-01 10:24:51 +1000

    BUG 1 - Solved - Dropdown

--- enviroment/courseApp/app/assets/javascripts/application.js
+++ enviroment/courseApp/app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery.min.js
+//= require bootstrap.min.js
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery
-//= require bootstrap
-//= require bootstrap-sprockets
