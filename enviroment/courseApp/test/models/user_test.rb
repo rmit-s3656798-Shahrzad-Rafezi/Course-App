ec54f056ca20b07245f216b173db12d86e04e95a
Shahrzad Rafezi <shahrzadrafezi@Shahrzads-MacBook-Pro.local>
2019-05-01 10:49:12 +1000

    fixed most of the test failures, excpet one

--- enviroment/courseApp/test/models/user_test.rb
+++ enviroment/courseApp/test/models/user_test.rb
@@ -31,11 +31,21 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email validation should accept valid addresses" do
-    invalid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
-    invalid_addresses.each do |valid_address|
+  test 'email validation should be accept valid addresses' do
+    valid_addresses = %w[user@rmit.edu.au USER@rmit.edu.au A_US-ER@rmit.edu.au
+                         first.last@rmit.edu.au alice+bob@rmit.edu.au]
+    valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
 
