f275326f870fcf1c001ef84938c9980f4837eebb
Shahryar Faraji <shahryar@1receipt.io>
2019-05-02 15:34:18 +1000

    added the minnium for email and name

--- enviroment/courseApp/app/models/user.rb
+++ enviroment/courseApp/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50, minimum: 4 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[rmit]+\.[edu]+\.[au]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255, minimum: 4 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   has_secure_password
