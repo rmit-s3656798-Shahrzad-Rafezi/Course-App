5ac7e50864a2e51c2e5617f18b592d5cc1fb29fa
Shahrzad Rafezi <shahrzadrafezi@Shahrzads-MacBook-Pro.local>
2019-05-09 15:11:06 +1000

    Hot fix test

--- enviroment/courseApp/test/models/user_test.rb
+++ enviroment/courseApp/test/models/user_test.rb
@@ -56,13 +56,6 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be be saved as lower-case" do
-    mixed_case_email = "Foo@rMiT.eDu.Au"
-    @user.email = mixed_case_email.downcase
-    @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email
-  end
-
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 5
     assert_not @user.valid?
