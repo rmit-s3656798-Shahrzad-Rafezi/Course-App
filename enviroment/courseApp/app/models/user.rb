fe738a497097fd706a183123d8e18dab7954da62
Shahrzad Rafezi <shahrzadrafezi@Shahrzads-MacBook-Pro.local>
2019-05-09 10:23:28 +1000

    Fixed the error display for rmit email and password, Also the image for home page display exactly thr same as the design

--- enviroment/courseApp/app/models/user.rb
+++ enviroment/courseApp/app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50, minimum: 4 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[rmit]+\.[edu]+\.[au]+\z/i
   validates :email, presence: true, length: { maximum: 255, minimum: 4 },
-            format: { with: VALID_EMAIL_REGEX },
+            format: { with: VALID_EMAIL_REGEX, message: "registration only open for RMIT staff" },
             uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, format: {with: /\A^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$\z/,
+                                  message: "must contains at least lowercase letter, an uppercase, a digit, a special character and 8+ characters."}
 
   # Returns the hash digest of the given string.
   def User.digest(string)
