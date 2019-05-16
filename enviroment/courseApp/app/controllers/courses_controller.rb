c4b2cdcb4c16aae686e3dd8cad8bbf3ac22c84c0
Shahryar Faraji <shahryar@1receipt.io>
2019-05-16 15:30:37 +1000

    updated controller

--- enviroment/courseApp/app/controllers/courses_controller.rb
+++ enviroment/courseApp/app/controllers/courses_controller.rb
@@ -1,2 +1,23 @@
 class CoursesController < ApplicationController
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+      render 'new'
+  end
+
+  private 
+
+  def course_params
+    parparams.require(:course).permit(:name, :prerequisite, :category,
+    :description)
+  end
+  
 end
--- enviroment/courseApp/db/schema.rb
+++ enviroment/courseApp/db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_001708) do
+ActiveRecord::Schema.define(version: 2019_05_16_051715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.string "prerequisite"
+    t.string "category"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,6 +36,11 @@ ActiveRecord::Schema.define(version: 2019_04_28_001708) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
