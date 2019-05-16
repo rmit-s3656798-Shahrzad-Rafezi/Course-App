60e50762f56c5247993ab2efb7e9d5b4f52da93e
Shahryar Faraji <shahryar@1receipt.io>
2019-05-16 20:15:36 +1000

    updated the category controller.

--- enviroment/courseApp/app/controllers/categories_controller.rb
+++ enviroment/courseApp/app/controllers/categories_controller.rb
@@ -1,2 +1,15 @@
 class CategoriesController < ApplicationController
+
+  def show
+    @categories = Category.find(params[:id])
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(course_params)
+      render 'new'
+  end
 end
