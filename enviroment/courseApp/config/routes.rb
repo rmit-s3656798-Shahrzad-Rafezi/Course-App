bfd4ce58ba08e48fc6a77ae4d8f418cec2187f63
Shahryar Faraji <shahryar@1receipt.io>
2019-05-18 13:32:51 +1000

    fixed the routes

--- enviroment/courseApp/config/routes.rb
+++ enviroment/courseApp/config/routes.rb
@@ -1,14 +1,51 @@
 Rails.application.routes.draw do
+  
+  get 'courses/index'
   get 'sessions/new'
+
+  resources :users do
+    member do
+      get :all_courses
+    end
+  end 
+
+  resources :courses do
+    member do
+      get :categories
+      post :category_add
+      post :category_remove
+      get :locations
+      post :location_add
+      post :location_remove
+      get :users
+      post :user_add
+      post :user_remove
+    end 
+  end
+
+  resources :locations do
+    member do
+      get :all_courses
+    end
+  end 
+
+  resources :categories do
+    member do
+      get :all_courses
+    end
+  end 
+
   get 'users/new'
+  
   root 'static_pages#home'
-  get  '/courses', to: 'static_pages#courses'
   get  '/contact', to: 'static_pages#contact'
+
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
   resources :users
-  resources :courses
+
 end
