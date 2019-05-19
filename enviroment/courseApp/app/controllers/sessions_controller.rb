d11e06637890f64dd64e3d570e843fb05f740bd5
Shahryar Faraji <shahryar@1receipt.io>
2019-05-19 14:00:03 +1000

    Impl user before action as well as friendly forwarding

--- enviroment/courseApp/app/controllers/sessions_controller.rb
+++ enviroment/courseApp/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       flash[:success] = "Welcome back!"
-      redirect_to user
+      redirect_back_or user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
