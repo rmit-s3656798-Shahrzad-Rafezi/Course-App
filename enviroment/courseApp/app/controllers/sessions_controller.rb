324cc82a733b56917a439a288e71b54f4b831305
Shahryar Faraji <shahryar@1receipt.io>
2019-05-19 14:48:32 +1000

    added the remember me function

--- enviroment/courseApp/app/controllers/sessions_controller.rb
+++ enviroment/courseApp/app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       flash[:success] = "Welcome back!"
-      redirect_back_or user
+      redirect_back_or @user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = "You succefully logged out."
     redirect_to root_url
   end
