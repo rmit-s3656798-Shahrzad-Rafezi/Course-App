0f261018b8231254068ded620df1511b94c8fb8e
Shahryar Faraji <shahryar@1receipt.io>
2019-05-21 12:32:05 +1000

    added the admin account

--- enviroment/courseApp/db/schema.rb
+++ enviroment/courseApp/db/schema.rb
@@ -21,11 +21,6 @@ ActiveRecord::Schema.define(version: 2019_05_21_014150) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "contacts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.string "prerequisite"
