6cf5e79395fa028a0ef920edf24c22afb96d9768
Shahryar Faraji <shahryar@1receipt.io>
2019-05-23 19:33:20 +1000

    revert back db

--- enviroment/courseApp/db/migrate/20190522002542_remove_old_foreign_key.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOldForeignKey < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key "courses", "categories"
-    remove_foreign_key "courses", "locations"
-    remove_foreign_key "courses", "users"
-    remove_foreign_key "courses", "votes"
-    remove_foreign_key "votes", "users"
-  end
-end
