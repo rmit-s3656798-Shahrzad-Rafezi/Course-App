435ddf0c07b2dd3281e12f5978e003b2ddba399b
Shahryar Faraji <shahryar@1receipt.io>
2019-05-25 13:56:58 +1000

    reverting back deleted files

--- enviroment/courseApp/config/routes.rb
+++ enviroment/courseApp/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server_error'
+
   get  '/addCourses',  to: 'courses#new'
   post '/addCourses',  to: 'courses#create'
 
