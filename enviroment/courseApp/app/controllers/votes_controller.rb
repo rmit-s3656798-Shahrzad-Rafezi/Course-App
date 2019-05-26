083f018a4a5e8ff665a7962b4aa0cff7168f315e
Shahryar Faraji <shahryar@1receipt.io>
2019-05-26 15:56:52 +1000

    Added the save in upvote, but with error

--- enviroment/courseApp/app/controllers/votes_controller.rb
+++ enviroment/courseApp/app/controllers/votes_controller.rb
@@ -3,8 +3,12 @@ class VotesController < ApplicationController
   def upvote
     @courses = Course.find(params[:id])
     @courses.votes.create(user_id: current_user)
-    flash[:success] = "Thanks for your vote"
-    redirect_to(courses_path)
+    if @vote.save
+        flash[:success] = "Thanks for your vote"
+        redirect_to(courses_path)
+      else
+        flash[:danger] = "Error :("
+    end
   end
 
   def downvote
