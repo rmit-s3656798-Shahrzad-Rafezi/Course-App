71d5a85c944222abc3f5685bc20ad8bfdf3ddb7f
Shahryar Faraji <shahryar@1receipt.io>
2019-05-26 14:48:16 +1000

    added votes parameter

--- enviroment/courseApp/app/controllers/votes_controller.rb
+++ enviroment/courseApp/app/controllers/votes_controller.rb
@@ -2,12 +2,18 @@ class VotesController < ApplicationController
 
   def upvote
     @courses = Course.find(params[:id])
-    @courses.votes.create(@user)
+    @courses.votes.create(user_id: current_user)
     redirect_to(courses_path)
   end
 
   # should be implemented for the admin functionality, which they can reset votes.
   def downvote
-  end    
+  end
   
+  private
+
+  def vote_params
+    params.require(:vote).permit(:user_id, :course_id, :type)
+  end
+
 end
