5500a7b3498e3102a2bf4e594c52b51ba90864b5
Shahryar Faraji <shahryar@1receipt.io>
2019-05-26 15:31:17 +1000

    Fixed the problem with the vote. Now only need to impl the couting of the vote as well as down vote

--- enviroment/courseApp/app/controllers/votes_controller.rb
+++ enviroment/courseApp/app/controllers/votes_controller.rb
@@ -3,10 +3,10 @@ class VotesController < ApplicationController
   def upvote
     @courses = Course.find(params[:id])
     @courses.votes.create(user_id: current_user)
+    flash[:success] = "Thanks for your vote"
     redirect_to(courses_path)
   end
 
-  # should be implemented for the admin functionality, which they can reset votes.
   def downvote
   end
   
