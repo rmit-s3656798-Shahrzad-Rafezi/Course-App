bcc78e03dda385082803aa99368e09cd82ca0d26
Shahryar Faraji <shahryar@1receipt.io>
2019-05-27 20:40:52 +1000

    Re-generate course table for vote

--- enviroment/courseApp/db/schema.rb
+++ enviroment/courseApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_052050) do
+ActiveRecord::Schema.define(version: 2019_05_27_055536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
