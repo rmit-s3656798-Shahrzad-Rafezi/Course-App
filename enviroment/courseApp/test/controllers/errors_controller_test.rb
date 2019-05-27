94a48550ee80c88801d9588f7e3e4c787a5a8868
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-05-27 22:02:31 +1000

    Test errors has been fixed!

--- enviroment/courseApp/test/controllers/errors_controller_test.rb
+++ enviroment/courseApp/test/controllers/errors_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should get not_found" do
-    get errors_not_found_url
-    assert_response :success
+    get not_found_url
+    assert_equal 404, status
   end
 
   test "should get internal_server_error" do
-    get errors_internal_server_error_url
-    assert_response :success
+    get server_error_url
+    assert_equal 500, status
   end
 
 end
