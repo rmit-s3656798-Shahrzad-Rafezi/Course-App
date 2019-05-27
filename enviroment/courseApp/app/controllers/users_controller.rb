bc7b54de238bfe52b4f780369110b70e40516f4e
Shahrzad Rafezi <shahrzad_rafezi@yahoo.com>
2019-05-27 20:23:59 +1000

    Managed to do delete course and delete user
    Just a note that you have to delete courses that belongs to user first, then you have to delete users. I'll figure out how to delete user and courses together.

--- enviroment/courseApp/app/controllers/users_controller.rb
+++ enviroment/courseApp/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :super_user,     only: [:index, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -36,11 +41,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def super_user
+      redirect_to(root_url) unless current_user.try(:superuser?)
     end
 
     # Before filters
