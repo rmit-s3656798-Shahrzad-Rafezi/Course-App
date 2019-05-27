78774cad880860b9b3bdcae7d0eca4b101e3ac80
Shahryar Faraji <shahryar@1receipt.io>
2019-05-27 23:29:59 +1000

    deleted the old static page contact form and replaced the footer link

--- enviroment/courseApp/config/routes.rb
+++ enviroment/courseApp/config/routes.rb
@@ -51,8 +51,6 @@ Rails.application.routes.draw do
   # end
 
   root 'static_pages#home'
-  get  '/contact', to: 'static_pages#contact'
-
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
