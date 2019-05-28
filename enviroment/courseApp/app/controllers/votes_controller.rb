9fa14dfb4bd5270d3810b69843f32c06089dedd2
Shahryar Faraji <shahryar@1receipt.io>
2019-05-28 10:34:11 +1000

    VOTES NOW SAVED!! JUST NEED TO TWEEK THE VIEW AND FIX THE VALIDATION.

--- enviroment/courseApp/app/controllers/votes_controller.rb
+++ enviroment/courseApp/app/controllers/votes_controller.rb
@@ -2,14 +2,16 @@ class VotesController < ApplicationController
 
   def upvote
     @course = Course.find(params[:id])
-    @course.votes.create
+    @course.votes.create(user_id: current_user.id)
+    @course.save
     flash[:success] = "Thanks for your vote"
     redirect_to(courses_path)
   end
 
   def downvote
     @course = Course.find(params[:id])
-    @course.votes.create
+    @course.votes.create(user_id: current_user.id)
+    @course.save
     flash[:success] = "Thanks for your vote"
     redirect_to(courses_path)
   end
